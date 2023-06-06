d58070be69519db6d7349b6b4870c5cf63d74a85
GilbertPrevost <30300745+GilbertPrevost@users.noreply.github.com>
2023-06-06 15:44:49 -0400

    Added 3 Sample Questions

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,12 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Is it possible to create a route from any job page?",
+        value: "Is it possible to create a route from any job page?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What is the difference between creating a route from dispatch view and list view?", 
+     value: "What is the difference between creating a route from dispatch view and list view?" },
+    { text: "Can we clone the maintenance?", value: "Can we clone the maintenance?" }
 ];
 
 interface Props {
